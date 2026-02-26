6dbe24fb5ddfc3411214d616810d6ac6b72efc8e
Yash Gupta <mind0bender@gmail.com>
2026-02-26 20:15:56 +0530

    theme check

--- extension/src/content/views/App.tsx
+++ extension/src/content/views/App.tsx
@@ -3,21 +3,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [show, setShow] = useState(false);
-  const toggle = () => setShow(!show);
-
-  return (
-    <div className="popup-container">
-      {show && (
-        <div className={`popup-content ${show ? "opacity-100" : "opacity-0"}`}>
-          <h1>HELLO CRXJS</h1>
-        </div>
-      )}
-      <button className="toggle-button" onClick={toggle}>
-        <img src={Logo} alt="CRXJS logo" className="button-icon" />
-      </button>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
