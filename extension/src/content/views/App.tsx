ecc5ea9a36497305294a46fef7c795692ac91195
Yash Gupta <mind0bender@gmail.com>
2026-02-28 18:22:37 +0530

    top posts page

--- extension/src/content/views/App.tsx
+++ extension/src/content/views/App.tsx
@@ -1,8 +1,7 @@
-import Logo from "@/assets/crx.svg";
-import { useState } from "react";
+import { JSX } from "react";
 import "./App.css";
 
-function App() {
+function App(): JSX.Element {
   return <></>;
 }
 
